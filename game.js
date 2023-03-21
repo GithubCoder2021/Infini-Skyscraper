a63ad1f217ddf07e339d123f9282d5e329ac99ef
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-03-21 13:41:06 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 let floors = -Infinity;
 let advance = 0;
+let speedCost = 0;
 
 setInterval(function(){
-  document.getElementById("floor").innerText = normal(floors);
+	document.getElementById("floor").innerText = normal(floors);
   document.getElementById("flPS").innerText = normal(advance);
+  document.getElementById("spCost").innerText = normal(speedCost);
   floors = addLogs(floors,divideLogs(advance,1.30103));
-},50);
+},50)
+
+function speedInc(){
+	if (floors >= speedCost){
+  	floors = subtractLogs(floors,speedCost);
+    advance = multiplyLogs(advance,0.30103);
+    speedCost = multiplyLogs(speedCost,0.3);
+    
+  }
+}

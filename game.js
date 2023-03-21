40c7e2b28e02d3488f6492d86aa5b7d87d22e80c
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-03-21 09:10:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let floors = -Infinity;
 let advance = 0;
 
 setInterval(function(){
-	document.getElementById("floor").innerText = normal(floors);
+  document.getElementById("floor").innerText = normal(floors);
   document.getElementById("flPS").innerText = normal(advance);
   floors = addLogs(floors,divideLogs(advance,1.30103));
 },50);
